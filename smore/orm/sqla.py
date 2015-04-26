1b43b5bd9cb1a498ee9d56a3e5dd212994430730
Steven Loria <sloria1@gmail.com>
2015-04-26 00:19:58 -0400

    First stab at a ModelConverter class

--- smore/orm/sqla.py
+++ smore/orm/sqla.py
@@ -1,6 +1,83 @@
-from marshmallow import Schema, validate
+from marshmallow import Schema, validate, fields
+import inspect
 
+import sqlalchemy as sa
 from sqlalchemy.orm.properties import ColumnProperty
+
+class ModelConversionError(Exception):
+    pass
+
+class ModelConverter(object):
+
+    def __init__(self):
+        self.converters = {}
+
+    def converts(self, *args):
+        """Decorator that registering a function that
+        converts the given SQLA column types to a
+        marshmallow field. The registered function receives
+        the sqlalchemy Property and a dictionary of keyword
+        arguments for the marshmallow field. The function
+        should return an instance of a marshmallow `Field`.
+
+        :params args: A list of SQLA column types
+        """
+        def _inner(func):
+            for sa_type in args:
+                self.converters[sa_type] = func
+            return func
+        return _inner
+
+    def fields_for_model(self, model):
+        result = {}
+        for prop in model.__mapper__.iterate_properties:
+            field = None
+            # Normal column -> marshmallow field
+            field = self._property2field(prop)
+            if field:
+                result[prop.key] = field
+        return result
+
+    @staticmethod
+    def fallback_converter(prop, field_kwargs, **extra):
+        FieldClass = Schema.TYPE_MAPPING[prop.columns[0].type.python_type]
+        return FieldClass(**field_kwargs)
+
+    def _get_converter_for_property(self, prop):
+        column = prop.columns[0]
+        types = inspect.getmro(type(column.type))
+
+        # Find a converter for the column's type in self.converters
+        for col_type in types:
+            if col_type in self.converters:
+                converter = self.converters[col_type]
+                break
+        else:
+            # Use fallback if there's no registered converter
+            if column.type.python_type in Schema.TYPE_MAPPING:
+                converter = self.fallback_converter
+            else:
+                raise ModelConversionError(
+                    'Could not find field converter for %s (%r).' % (prop.key, types[0]))
+        return converter
+
+    def _property2field(self, prop):
+        if isinstance(prop, ColumnProperty):
+            # Get field class based on python type
+            converter_func = self._get_converter_for_property(prop)
+            field_kwargs = field_kwargs_for_property(prop)
+            return converter_func(prop, field_kwargs=field_kwargs)
+        # TODO: relationships
+
+
+default_converter = ModelConverter()
+converts = default_converter.converts
+fields_for_model = default_converter.fields_for_model
+
+@converts(sa.String, sa.Unicode)
+def conv_String(prop, field_kwargs, **extra):
+    return fields.String(**field_kwargs)
+
 
 def field_kwargs_for_property(prop):
     column = prop.columns[0]
@@ -29,18 +106,9 @@ def field_kwargs_for_property(prop):
 
 def property2field(prop):
     if isinstance(prop, ColumnProperty):
-        FieldClass = Schema.TYPE_MAPPING[prop.columns[0].type.python_type]
+        column = prop.columns[0]
+        # Get field class based on python type
+        FieldClass = Schema.TYPE_MAPPING[column.type.python_type]
         kwargs = field_kwargs_for_property(prop)
         return FieldClass(**kwargs)
     # TODO: Relationships
-
-def fields_for_model(model):
-    """Generate a dictionary of fields given a SQLA model."""
-    result = {}
-    for prop in model.__mapper__.iterate_properties:
-        field = None
-        # Normal column -> marshmallow field
-        field = property2field(prop)
-        if field:
-            result[prop.key] = field
-    return result
