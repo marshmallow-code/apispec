3c4c94614e5297aebfe1038b25c9cf8c7f71d945
Steven Loria <sloria1@gmail.com>
2015-09-27 17:08:58 -0400

    Update dev-requirements; use twine for uploading to pypi

--- tasks.py
+++ tasks.py
@@ -77,12 +77,10 @@ def readme(browse=False):
 @task
 def publish(test=False):
     """Publish to the cheeseshop."""
-    try:
-        __import__('wheel')
-    except ImportError:
-        print('wheel required. Run `pip install wheel`.')
-        sys.exit(1)
+    clean()
     if test:
-        run('python setup.py register -r test sdist bdist_wheel upload -r test', echo=True)
+        run('python setup.py register -r test sdist bdist_wheel', echo=True)
+        run('twine upload dist/* -r test', echo=True)
     else:
-        run('python setup.py register sdist bdist_wheel upload', echo=True)
+        run('python setup.py register sdist bdist_wheel', echo=True)
+        run('twine upload dist/*', echo=True)
