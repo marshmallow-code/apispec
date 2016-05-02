0e0b2f6b98030a3defc65e800dc9c3882d0ef581
Jared Deckard <jared.deckard@gmail.com>
2016-05-02 00:28:04 -0500

    Add examples to the mm schema path helper

--- apispec/ext/marshmallow/__init__.py
+++ apispec/ext/marshmallow/__init__.py
@@ -52,6 +52,63 @@ def schema_definition_helper(spec, name, schema, **kwargs):
 def schema_path_helper(spec, view, **kwargs):
     """Path helper that allows passing a Schema as a response. Responses can be
     defined in a view's docstring.
+    ::
+
+        from pprint import pprint
+
+        from my_app import Users, UserSchema
+
+        class UserHandler:
+            def get(self, user_id):
+                '''Get a user endpoint.
+                ---
+                description: Get a user
+                responses:
+                    200:
+                        description: A user
+                        schema: UserSchema
+                '''
+                user = Users.get(id=user_id)
+                schema = UserSchema()
+                return schema.dumps(user)
+
+        urlspec = (r'/users/{user_id}', UserHandler)
+        spec.add_path(urlspec=urlspec)
+        pprint(spec.to_dict()['paths'])
+        # {'/users/{user_id}': {'get': {'description': 'Get a user',
+        #                               'responses': {200: {'description': 'A user',
+        #                                                   'schema': {'$ref': '#/definitions/User'}}}}}}
+
+    ::
+
+        from pprint import pprint
+
+        from my_app import Users, UserSchema
+
+        class UsersHandler:
+            def get(self):
+                '''Get users endpoint.
+                ---
+                description: Get a list of users
+                responses:
+                    200:
+                        description: A list of user
+                        schema:
+                            type: array
+                            items: UserSchema
+                '''
+                users = Users.all()
+                schema = UserSchema(many=True)
+                return schema.dumps(users)
+
+        urlspec = (r'/users', UsersHandler)
+        spec.add_path(urlspec=urlspec)
+        pprint(spec.to_dict()['paths'])
+        # {'/users': {'get': {'description': 'Get a list of users',
+        #                     'responses': {200: {'description': 'A list of users',
+        #                                         'schema': {'type': 'array',
+        #                                                    'items': {'$ref': '#/definitions/User'}}}}}}}
+
     """
     operations = (
         load_operations_from_docstring(view.__doc__) or
