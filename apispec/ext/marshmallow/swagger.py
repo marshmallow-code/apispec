c540eda93686e4d9487f9d8cc58f38233cd3c74c
Daniel Radetsky <dradetsky@gmail.com>
2017-08-07 14:28:25 -0700

    oops, type not reqd in both cases

--- apispec/ext/marshmallow/swagger.py
+++ apispec/ext/marshmallow/swagger.py
@@ -88,7 +88,7 @@ def _observed_name(field, name):
 
 
 def _get_json_type_for_field(field):
-    if hasattr(type(field), CUSTOM_FIELD_MAPPING_ATTR):
+    if hasattr(field, CUSTOM_FIELD_MAPPING_ATTR):
         json_type, fmt = getattr(field, CUSTOM_FIELD_MAPPING_ATTR)
     else:
         json_type, fmt = FIELD_MAPPING.get(type(field), ('string', None))
