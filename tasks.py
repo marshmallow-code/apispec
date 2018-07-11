fd9ef4b30ef09d52c120e9e8ab2461cbce939dfd
Steven Loria <sloria1@gmail.com>
2018-07-10 22:37:33 -0400

    Add --show-diff-on-failure

--- tasks.py
+++ tasks.py
@@ -28,7 +28,7 @@ def test(ctx, watch=False, last_failing=False):
 @task
 def syntax(ctx):
     """Run flake8 on codebase."""
-    ctx.run('pre-commit run --all-files', echo=True)
+    ctx.run('pre-commit run --all-files --show-diff-on-failure', echo=True)
 
 @task
 def watch(ctx):
