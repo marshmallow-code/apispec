d5a9b6b2a4a4fbd2acf115baf185cfeb9db83839
Steven Loria <sloria1@gmail.com>
2018-10-28 15:48:26 -0400

    Add python 3.7 classifier
    [skip ci]

--- setup.py
+++ setup.py
@@ -84,6 +84,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     project_urls={
