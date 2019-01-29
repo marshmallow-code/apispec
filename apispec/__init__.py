976d633198acf9d5623aa323c117c7d1ef58a26d
Steven Loria <sloria1@gmail.com>
2019-01-29 18:48:11 -0500

    Remove __authors__ and __license__
    These are unnecessary and duplicative

--- apispec/__init__.py
+++ apispec/__init__.py
@@ -5,10 +5,6 @@ from .core import APISpec
 from .plugin import BasePlugin
 
 __version__ = '1.0.0b6'
-__author__ = 'Steven Loria, Jérôme Lafréchoux, and contributors'
-__license__ = 'MIT'
-
-
 __all__ = [
     'APISpec',
     'BasePlugin',
