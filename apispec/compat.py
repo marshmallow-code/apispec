4d3d2166e1c0a2d29258700445f1df953cc01970
Steven Loria <sloria1@gmail.com>
2019-02-02 13:33:18 -0500

    Remove unused compat code

--- apispec/compat.py
+++ apispec/compat.py
@@ -5,18 +5,12 @@ import sys
 PY2 = int(sys.version[0]) == 2
 
 if PY2:
-    text_type = unicode
-    binary_type = str
-    string_types = (str, unicode)
     unicode = unicode
     basestring = basestring
     iterkeys = lambda d: d.iterkeys()
     itervalues = lambda d: d.itervalues()
     iteritems = lambda d: d.iteritems()
 else:
-    text_type = str
-    binary_type = bytes
-    string_types = (str,)
     unicode = str
     basestring = (str, bytes)
     iterkeys = lambda d: d.keys()
