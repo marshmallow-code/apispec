33f0c277429499e023ab6eac32bcdd0feed1c59a
Jérôme Lafréchoux <jlafrechoux@nobatek.com>
2019-04-02 10:29:51 +0200

    Test utils.reference_path

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -23,3 +23,14 @@ class TestOpenAPIVersion:
         assert ver.patch == 1
         assert ver.vstring == "3.0.1"
         assert str(ver) == "3.0.1"
+
+
+def test_reference_path():
+    assert utils.reference_path('schema', 2) == '#/definitions/'
+    assert utils.reference_path('parameter', 2) == '#/parameters/'
+    assert utils.reference_path('response', 2) == '#/responses/'
+    assert utils.reference_path('security_scheme', 2) == '#/securityDefinitions/'
+    assert utils.reference_path('schema', 3) == '#/components/schemas/'
+    assert utils.reference_path('parameter', 3) == '#/components/parameters/'
+    assert utils.reference_path('response', 3) == '#/components/responses/'
+    assert utils.reference_path('security_scheme', 3) == '#/components/securitySchemes/'
